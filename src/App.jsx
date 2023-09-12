bd7b135e781d9a6122025ff8616f6fe87e364240
siamislam07 <siamislam1970@gmail.com>
2023-09-12 17:33:12 +0600

    Bookmard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
 import BookMarks from './Components/BookMarks/BookMarks'
 import Header from './Components/Header/Header'
 
 function App() {
+  const [bookmars, setBookmarks] = useState([])
 
+  const handleAddToBookmark = blog =>{
+    console.log(blog);
+  }
 
   return (
     <>
 
       <Header></Header>
 
-      <div className='md:flex '>
-        <Blogs></Blogs>
+      <div className='md:flex max-w-7xl mx-auto' >
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
         <BookMarks></BookMarks>
       </div>
 
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,16 +1,38 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
-    const {title} = blog
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, author, author_img, reading_time, posted_date, hashtags } = blog
     return (
-        <div>
-            <h2 className='text-4xl'> {title.length}</h2>
+        <div className='mb-20'>
+            <img className='w-full mb-8 rounded-lg'  src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex '>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={()=>handleAddToBookmark(blog)} className='ml-2 text-2xl text-red-500'><FaBookmark /></button>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func
 }
 
 export default Blog;
