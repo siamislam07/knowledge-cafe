8e6eb308044c93c0697fb2d9ad2a84bff8b5b362
siamislam07 <siamislam1970@gmail.com>
2023-09-12 11:08:12 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import BookMarks from './Components/BookMarks/BookMarks'
+import Header from './Components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Header></Header>
+
+      <div className='md:flex '>
+        <Blogs></Blogs>
+        <BookMarks></BookMarks>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/BookMarks/BookMarks.jsx
@@ -0,0 +1,11 @@
+
+
+const BookMarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h2>Book Marks</h2>
+        </div>
+    );
+};
+
+export default BookMarks;
